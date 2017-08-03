909ff1ac016ee2b64b6e6071e5561ef3d754af7d
zxcsheryl <sxc.zhang@gmail.com>
2017-08-03 12:37:49 -0400

    This function creates a special "matrix" object     that can cache its inverse.

--- cachematrix.R
+++ cachematrix.R
@@ -1,5 +1,6 @@
 ## Put comments here that give an overall description of what your
 ## functions do
+## A pair of functions that cache the inverse of a matrix
 
 ## Write a short comment describing this function
 
